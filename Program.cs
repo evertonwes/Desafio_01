647ae2f8d5dd6630daccb4f93d306c4effaa35ec
evertonwes <88938675+evertonwes@users.noreply.github.com>
2021-10-07 16:53:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Int - Armazena numeros inteiros
+            // Float - Armazena numeros com casas decimais ====> Double / decimal
+            // bool - Armazenas variável do tipo verdadeiro ou falso ( true ou folse)
+            // String - Armazena textos dentro "exemplo"
+            // Char- Armazena uma caracter dentro de 'exemplo'
+
+            int segundaguerramundial = 1942;
+            string cor_favorita = "Vermelho";
+            float velocidadef1 = 294.48f;
+            bool segundaguerramundialaconteceu = true;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Meu nome é Everton Weslley");
+
+
+            Console.ReadLine();
+        }
+    }
+}
